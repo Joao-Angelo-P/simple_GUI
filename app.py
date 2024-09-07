7ad0ee6821ae2414007e015ad3fbc4b2d3ffef4d
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-09-07 19:07:06 -0400

    fix: Create a Frame object in Master Application

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from db import conn, cur
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack(padx=0, pady=0)
         l = tk.Label(self, text="Login:")
         l2 = tk.Label(self, text="Senha:")
 
@@ -11,15 +12,15 @@ class App(tk.Tk):
         self.en2 = tk.Entry(self,)
 
         btn = tk.Button(self, text="Entrar", command=self.login)
-        l.grid(row=0, column=0, columnspan=1, pady=5)
-        self.en.grid(row=0, column=1,pady=5)
-        l2.grid(row=1, column=0, columnspan=1)
-        self.en2.grid(row=1, column=1)
-        btn.grid(row=2, column=3)
+        l.grid(row=0, column=0, columnspan=1,pady=5)
+        self.en.grid(row=0, column=1, pady=5)
+        l2.grid(row=1, column=0, columnspan=1, pady=5)
+        self.en2.grid(row=1, column=1, pady=5)
+        btn.grid(row=2, column=1, pady=10)
 
         # Configuração da Janela
-        self.geometry("500x500")
-        self.title("Janela")
+        master.geometry("500x500")
+        master.title("Janela")
 
     def login(self):
         s1, s2 = (self.en.get(), self.en2.get())
@@ -31,9 +32,11 @@ class App(tk.Tk):
         
             if s1 == login and s2 == senha:
                 wn = tk.Toplevel(self)
-                l = tk.Label(wn, text="Bem-Vindo").pack()
+                l = tk.Label(wn, text="Bem-Vindo\nInsira novo Usuario").pack()
+
                 wn.title("Nova Janela")
                 wn.geometry("300x250")
+
             else:
                 wn = tk.Toplevel(self)
                 l = tk.Label(wn, text="Acesso Negado", ).pack()
@@ -41,5 +44,6 @@ class App(tk.Tk):
                 wn.geometry("300x300")
 
 if __name__ == '__main__':
-    app = App()
+    root = tk.Tk()
+    app = App(root)
     app.mainloop()
