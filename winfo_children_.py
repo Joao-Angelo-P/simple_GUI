73a9ce1cb191c91346e51b12111af17bf3fc1ee0
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-02-23 12:39:33 -0400

    Update winfo_children_.py
    retorna um dicionário ou uma lista

--- winfo_children_.py
+++ winfo_children_.py
@@ -1,18 +1,26 @@
 from tkinter import Tk, Label, Entry
 from collections import defaultdict
+from typing import get_type_hints
 
 class TESTE(Tk):
     def __init__(self):
         super(TESTE, self).__init__()
 
 
-    def winfo_children(self):
+    def winfo_children(self, _list: bool = None, _reverse: bool = True) -> dict | list:
         objs = super(TESTE, self).winfo_children()
         d = defaultdict(list)
         for x in objs:
             d[x.__class__.__name__].append(x)
 
         Label(self, text=f"Sao {len(objs)} widgets\n'Label': {d['Label']}\n'Entry':{d['Entry']}").pack()
+
+        if _list:
+            return sorted(d.items(), reverse=_reverse)
+
+        else:
+            return d
+
 
 teste = TESTE()
 
@@ -23,5 +31,8 @@ Entry(teste)
 Entry(teste)
 Entry(teste)
 
-teste.winfo_children()
+print(teste.winfo_children(True, False))
+print(teste.winfo_children())
+print(get_type_hints(teste.winfo_children))
+
 teste.mainloop()
